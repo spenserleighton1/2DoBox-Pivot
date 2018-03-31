f3fc9461b090b64a1c6ceaee99ccd83decc5edbe
Arram Mandel <airum82@gmail.com>
2018-03-31 15:54:52 -0600

    Finalized working template for saving ideas and made input values clear on click

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-var $titleInput = $('');
-var $bodyInput = $('');
-var $saveButton = $('');
-
-
-
+var $titleInput = $('.idea-title');
+var $bodyInput = $('.body');
+var $saveButton = $('.save');
 
 $saveButton.on('click', addItemToList);
 
-function addItemToList () {
-	// event.preventdefault();
-	console.log($titleInput.val(), $bodyInput.val());
-	
-	var title = $bodyInput.val();
-	var body = $titleInput.val();
+function addItemToList(event) {
+  var $deleteButton = $('.delete-button');
+  var $title = $titleInput.val();
+  var $body = $bodyInput.val();
+	event.preventDefault();
 
 // template literal
 
-		$ideaList.prepend (
-		`${title}
- 		<button class = 'delete-button'> <img src = 'images.delete.svg'></button>
- 		 ${body}
- 		 <button class = 'upvote-button'><img src = 'images.upvote.svg'></button>
- 		 <button class = 'downvote-button'><img src = 'images.downvote.svg'></button>
- 		 <h4>quality: </h4>
+		$('section').prepend (
+      `<article>
+		  <button class = 'delete-button'></button>
+      <h2>${$title}</h2>
+ 		   <p>${$body}</p>
+ 		   <button class = 'upvote-button'></button>
+ 		   <button class = 'downvote-button'></button>
+ 		   <h4>quality: </h4>
+       </article>
 		`)
-
+    $('article').on('click', '.delete-button', deleteButtonClicked);
+    clearInputs();
 }
 
-
-
-
-function deleteButtonClicked () {
+function deleteButtonClicked (event) {
 	$(this).parent().remove();
 }
+
+function clearInputs() {
+  event.preventDefault();
+  $titleInput.val('');
+  $bodyInput.val('');
+}
+
