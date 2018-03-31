41cdfc3558b0e8b6729ad70e0da339f4bafacd48
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-03-31 16:57:00 -0600

    upvote and downvote buttons working

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var $titleInput = $('.idea-title');
 var $bodyInput = $('.body');
 var $saveButton = $('.save');
 
+
 $saveButton.on('click', addItemToList);
 
 function addItemToList(event) {
@@ -10,8 +11,6 @@ function addItemToList(event) {
   var $body = $bodyInput.val();
 	event.preventDefault();
 
-// template literal
-
 		$('section').prepend (
       `<article>
 		  <button class = 'delete-button'></button>
@@ -19,11 +18,42 @@ function addItemToList(event) {
  		   <p>${$body}</p>
  		   <button class = 'upvote-button'></button>
  		   <button class = 'downvote-button'></button>
- 		   <h4>quality: </h4>
+ 		   <h4>quality:<span class='quality'>swill</span></h4>
        </article>
 		`)
     $('article').on('click', '.delete-button', deleteButtonClicked);
     clearInputs();
+		$('article').on('click', '.upvote-button', upVoteClicked);
+		$('article').on('click', '.downvote-button', downVoteClicked);
+}
+function upVoteClicked() {
+	var $upvoteButton = $('.upvote-button')
+	var $quality = $('.quality');
+ 	if ($quality.text() ==='swill') {
+ 		$quality.text('plausible');
+ 	} else if ($quality.text() === 'plausible') {
+ 		$quality.text('genius');
+ 	} else {
+ 		return
+ 	} 
+ }
+
+
+
+ 
+
+function downVoteClicked(){
+	var $downvoteButton = $('.downvote-button')
+	var $quality = $('.quality')
+
+ 	if ($quality.text() === 'genius') {
+ 		$quality.text('plausible');
+ 	} else if ($quality.text() === 'plausible') {
+ 		$quality.text('swill');
+ 	} else {
+ 		return
+ 	} 
+
 }
 
 function deleteButtonClicked (event) {
