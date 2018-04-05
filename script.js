1341557d11f2d21cb806dc876de90e42a2a2a6ec
Arram Mandel <airum82@gmail.com>
2018-04-05 11:53:31 -0600

    Working on fixing content edit issue in local storage

--- script.js
+++ script.js
@@ -7,48 +7,86 @@ $('section').on('click', '.delete-button', deleteButtonClicked);
 $('section').on('click', '.upvote-button', upVoteClicked);
 $('section').on('click', '.downvote-button', downVoteClicked);
 
-//save editable fields.
-//add aria label to inputs.
-//Need to do DTR.
+function buildMarkup(idGen, title, body) {
+     return `<article id=${idGen}>
+      <button class = 'delete-button'></button>
+      <h2>${title}</h2>
+       <p>${body}</p>
+       <button class = 'upvote-button' aria-label='upvote'></button>
+       <button class = 'downvote-button' aria-label = 'downvote' ></button>
+       <h4>quality:<span class='quality' role='quality'>swill</span></h4>
+       <hr>
+       </article>`
+};
+
 function addItemToList(event) {
   var idGen = Date.now();
-  // console.log(idGen);
   var $deleteButton = $('.delete-button');
   var $title = $titleInput.val();
   var $body = $bodyInput.val();
 	event.preventDefault();
-
-		var markUp =
-      `<article id=${idGen}>
-		  <button class = 'delete-button'></button>
-      <h2 contenteditable="true">${$title}</h2>
- 		   <p contenteditable="true">${$body}</p>
- 		   <button class = 'upvote-button'></button>
- 		   <button class = 'downvote-button'></button>
- 		   <h4>quality:<span class='quality'>swill</span></h4>
-       </article>
-		`;
-    var $currentArticle = $(markUp);
-    $('section').prepend($currentArticle);
-    clearInputs();
-    // $currentArticle.on('click', '.delete-button', deleteButtonClicked);
-		// $currentArticle.on('click', '.upvote-button', upVoteClicked);
-		// $currentArticle.on('click', '.downvote-button', downVoteClicked);
-    localStorage.setItem(idGen, markUp);
-}
+	var markUp = buildMarkup(idGen, $title, $body)
+  var $currentArticle = $(markUp);
+  $('section').prepend($currentArticle);
+  clearInputs();
+  localStorage.setItem(idGen, markUp);
+};
 
 $( document ).ready(function(event) {
-  for(var i =0; i < localStorage.length; i++){
+  for(var i = 0; i < localStorage.length; i++){
   $('section').append(localStorage.getItem(localStorage.key(i)));
   }  
-})
 
-function saveUserEdits() {
-  var getIdea = localStorage.getItem(JSON.parse(key(0)));
-  return getIdea;
 
+$('h2').on('click',function(event) {
+  var key1 = localStorage.getItem(localStorage.key($(this).parent().attr('id')));
+  console.log(key1);
+  var newText = $(this).text();
+  var addInput = `<input type="text" value=${newText} class="changeTitle">`;
+  if ($(this).children().length === 0) {
+    $(this).text('');
+    $(this).append(addInput);
+    $(this).children().focus();
+    var key = $(this).parent().attr('id');
+    updateTitle(key);
+  } else {
+    return
+  }
+
+});
+
+function updateTitle(key) {
+  $('.changeTitle').on('blur',function(event) {
+  var title = $(this).val();
+  var body = $(this).parent().siblings('p');
+  var markUp = buildMarkup(key, title, body);
+  localStorage.setItem(key, markUp);
+  });
 }
 
+$('p').on('click', function(event) {
+  var newText = $(this).text();
+  var addInput = `<input type="text" value=${newText} class="changeContent">`;
+   if ($(this).children().length === 0) {
+    $(this).text('');
+    $(this).append(addInput);
+    $(this).children().focus();
+    var key = $(this).parent().attr('id');
+    updateBody(key);
+  } else {
+    return
+}
+});
+})
+
+function updateBody(key) {
+  $('.changeContent').on('blur', function(event) {
+  var body = $(this).val();
+  var title = $(this).parent().siblings('h2').text();
+  var markUp = buildMarkup(key, title, body);
+  localStorage.setItem(key, markUp);
+  })
+}
 
 function upVoteClicked(event) {
 	var $upvoteButton = $(event.target);
@@ -86,6 +124,3 @@ function clearInputs() {
   $titleInput.val('');
   $bodyInput.val('');
 }
-
-
-
